3c243a5e630812fdd4aba3cee2977ff761680ac6
Tobias Aigner <tobias.aigner@gmail.com>
2014-02-05 22:16:08 +0100

    added some more vector operations

--- src/cljs_experiments/vector.cljs
+++ src/cljs_experiments/vector.cljs
@@ -25,3 +25,11 @@
   (if (> (mag-squared v) (* factor factor))
     (mult (normalize v) factor)
     v))
+
+(defn distance [[x1 y1] [x2 y2]]
+  (let [dx (- x1 x2)
+        dy (- y1 y2)]
+    (Math/sqrt (+ (* dx dx) (* dy dy)))))
+
+(defn div [[x y] n]
+  [(/ x n) (/ y n)])
