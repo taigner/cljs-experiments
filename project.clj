9c180dfcfcfc4a521c61c42dd2c2226171cd20de
Tobias Aigner <tobias.aigner@gmail.com>
2014-02-12 22:32:17 +0100

    updated ClojureScript version

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://github.com/taigner"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]]
+                 [org.clojure/clojurescript "0.0-2156"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "cljs-experiments"
               :source-paths ["src"]
               :compiler {
